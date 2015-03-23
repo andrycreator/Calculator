dfdc699c1212d9170c8dfb28dfef3765ad22e42c
andrii <a.creator30@gmail.com>
2015-03-23 22:53:23 +0200

    add change

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,10 +1,42 @@
 package calculator;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator {
 
+public class Calculator extends Frame implements ActionListener {
+    
+    public Calculator() {
+    }
+    
+    public Calculator(String title) {
+        super(title);
+    }
+
+    
     public static void main(String[] args) {
-            
+        Frame calc = new Calculator("First frame");
+        calc.setSize(400, 400);
+        calc.setLayout(new FlowLayout());
+        calc.setVisible(true);
+        
+        
+        Button button = new Button("Test");
+        button.setSize(20, 60);
+        calc.add(button);
+        
+        Label label = new Label();
+        calc.add(label);
+        
                 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
